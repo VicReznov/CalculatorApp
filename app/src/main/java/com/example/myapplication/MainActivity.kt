f860720e2f3f8426bacc2160579985a3f9c55e6b
Reznov <reznov03048@gmail.com>
2022-08-03 00:02:53 +0900

    add buttons, make functions of clear, decimal

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,15 +3,38 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.main_textview_input)
     }
 
     fun onDigit(view:View){
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
+    }
+
+    fun onClear(view: View){
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
